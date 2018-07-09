c9b0ef11a8b0c891c780da665b906d7b46c895e9
alonsocoding <alonsodelm@gmail.com>
2018-07-08 20:29:55 -0600

    New Dto for Updating User

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,12 +5,13 @@ using DatingApp.API.Dtos;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System;
 
 namespace DatingApp.API.Controllers
 {
     // Only Authorize users can send request to this API
     // You can communicate with the API using the URL api/user
-    
     [Authorize]
     [Route("api/[controller]")]
     public class UserController : Controller
@@ -23,6 +24,8 @@ namespace DatingApp.API.Controllers
             _mapper = mapper;
             _repo = repo;
         }
+
+        // Method created to Get all the Users in the current table
         [HttpGet]
         public async Task<IActionResult> GetUsers()
         {
@@ -31,12 +34,35 @@ namespace DatingApp.API.Controllers
             return Ok(usersToReturn);
         }
 
+        // Method created to Get a User in the current table based in the id property
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
         {
             var user = await _repo.GetUser(id);
             var userToReturn = _mapper.Map<UserForDetailDto>(user);
             return Ok(userToReturn);
+        }
+
+        // Method created to Update a User in the current table based in the id property
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, [FromBody] UserForUpdateDto userForUpdateDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var currentUserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            var userFromRepo = await _repo.GetUser(id);
+
+            if (userFromRepo == null)
+                return NotFound($"Could not find user with an ID of {id}");
+            if (currentUserId != userFromRepo.Id)
+                return Unauthorized();
+
+                _mapper.Map(userForUpdateDto, userFromRepo);
+            if (await _repo.SaveAll())
+                return NoContent();
+
+            throw new Exception($"Updating user {id} failed on save");
         }
 
 
